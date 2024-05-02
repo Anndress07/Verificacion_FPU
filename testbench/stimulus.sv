b6c05ad9d6aab07dd63469a08620a14f115150ea
Anndress07 <92402988+Anndress07@users.noreply.github.com>
2024-05-02 13:45:51 -0600

    Update stimulus.sv

--- testbench/stimulus.sv
+++ testbench/stimulus.sv
@@ -1,8 +1,7 @@
 class stimulus;
   rand   logic[31:0]  valueA  ;
   rand   logic[31:0]  valueB  ;
-  shortreal floatA = valueA;
-  shortreal floatB = valueB;
+
   
   //constraint distribution {value dist { 0  := 1 , 1 := 1 }; } 
 endclass
