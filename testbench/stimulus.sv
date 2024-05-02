29f62cc65de61f8ecefb9c28fdd3e852172768b9
Anndress07 <92402988+Anndress07@users.noreply.github.com>
2024-05-02 13:39:47 -0600

    Create stimulus.sv

---
+++ testbench/stimulus.sv
@@ -0,0 +1,8 @@
+class stimulus;
+  rand   logic[31:0]  valueA  ;
+  rand   logic[31:0]  valueB  ;
+  shortreal floatA = valueA;
+  shortreal floatB = valueB;
+  
+  //constraint distribution {value dist { 0  := 1 , 1 := 1 }; } 
+endclass
